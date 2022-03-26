76a785fb950b0ae98890c4d5e38c3a1b341599f5
hj <hj@qq.com>
2022-03-26 12:20:13 +0800

    hot-first

--- src/main/java/com/hj/git_test.java
+++ src/main/java/com/hj/git_test.java
@@ -3,5 +3,7 @@ package com.hj;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("ni hao");
+        System.out.println("ni hao");
+        System.out.println("ni hao");
     }
 }
